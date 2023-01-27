9b66bf0745055bd8771ee4817e16e7b04a10fde6
Edgar Milla <edgar.milla#gmail.com>
2023-01-26 20:12:10 -0800

    fixed server

--- sensor.py
+++ sensor.py
@@ -3,7 +3,7 @@ import websockets
 import asyncio
 
 # Server data
-address = "10.0.0.38"
+address = "atom-radpi-01.local"
 PORT = 7890
 print("Server listening on Port " + str(PORT))
 
@@ -31,6 +31,6 @@ async def echo(websocket, path):
         connected.remove(websocket)
 
 # Start the server
-start_server = websockets.serve(echo, "localhost", PORT)
+start_server = websockets.serve(echo, address, PORT)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
