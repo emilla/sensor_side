c4f188a38dd33e381ea0517c3c90307062ae34be
Edgar Milla <edgar.milla#gmail.com>
2023-01-27 19:08:21 -0800

    changed pins

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 import time
 
-sensor = DistanceSensor(echo=4, trigger=17)
+sensor = DistanceSensor(echo=17, trigger=4)
 
 #function to get the distance in cm and time it was taken as a JSON string
 def get_distance():
