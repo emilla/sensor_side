5d82058f2d2f34c0a7a658cb9ec1b5f385437a81
Edgar Milla <edgar.milla#gmail.com>
2023-01-27 19:57:22 -0800

    fixed time

--- distance_sensor.py
+++ distance_sensor.py
@@ -6,7 +6,7 @@ sensor = DistanceSensor(echo=18, trigger=17)
 #function to get the distance in cm and time it was taken as a JSON string
 def get_distance():
     distance = sensor.distance * 100
-    time = sensor.timestamp
+    time = time.time()
     JSON = '{"distance":"{}", "time":"{}"}'.format(distance, time)
     return JSON
 
